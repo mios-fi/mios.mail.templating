c9839f402e81cdc0ef61e64cfab3996d53a03553
jbeijar <jbeijar@mios.fi>
2012-01-18 13:06:54 +0200

    Add imageresizing project
    Fix package references
    Moved templating test library to correct folder

---
+++ Tests/Integration.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Net.Mail;
+using Mios.Mail.Templating;
+using Should;
+using Xunit;
+
+namespace Tests {
+	public class Integration {
+		[Fact]
+		public void CanTransformTypedMessageToEmail() {
+			var factory = new TypeBasedRazorEmailTemplateFactory();
+			var template = factory.Create<User>();
+			var from = new MailAddress("info@example.com");
+			var to = new MailAddress("bill@example.com");
+			MailMessage message;
+			message = template.Transform(new User { Name = "Bob" }, to, from);
+			message.To.ShouldContain(to);
+			message.From.ShouldEqual(from);
+			message.Subject.ShouldEqual("Hello Bob");
+			message.Body.ShouldEqual("Hi there Bob, how are you today?");
+			using(var reader = new StreamReader(message.AlternateViews[0].ContentStream)) {
+				var body = reader.ReadToEnd();
+				body.ShouldContain("<h1>Hi Bob</h1>");
+				body.ShouldContain("<title>Hello Bob</title>");
+			}
+		}
+		[Fact]
+		public void CanTransformDynamicMessageToEmail() {
+			var factory = new RazorEmailTemplateFactory();
+			var template = factory.Create("Templates\\User.cstxt","Templates\\User.cshtml");
+			var from = new MailAddress("info@example.com");
+			var to = new MailAddress("bill@example.com");
+			MailMessage message;
+			message = template.Transform(new User {Name = "Bob"}, to, from);
+			message.To.ShouldContain(to);
+			message.From.ShouldEqual(from);
+			message.Subject.ShouldEqual("Hello Bob");
+			message.Body.ShouldEqual("Hi there Bob, how are you today?");
+			using(var reader = new StreamReader(message.AlternateViews[0].ContentStream)) {
+				var body = reader.ReadToEnd();
+				body.ShouldContain("<h1>Hi Bob</h1>");
+				body.ShouldContain("<title>Hello Bob</title>");
+			}
+		}
+		public class User {
+			public string Name { get; set; }
+		}
+	}
+}
---
+++ Tests/RazorTemplateTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Dynamic;
+using System.IO;
+using Mios.Mail.Templating;
+using Xunit;
+
+namespace Tests {
+	public class RazorTemplateTests {
+		[Fact]
+		public void CanProcessDynamicModel() {
+			var factory = new RazorTemplateFactory();
+			const string str = "Hello @Model.Name you are number @Model.Count in line.";
+			dynamic model = new ExpandoObject();
+			model.Count = 42;
+			model.Name = "Bob";
+			var template = factory.CreateDynamicTemplate(new StringReader(str));
+			Assert.Equal("Hello Bob you are number 42 in line.", template.Execute(model) as string);
+		}
+	}
+}
