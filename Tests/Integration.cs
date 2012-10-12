5d2511975a4cc28196571a8872d31e3b9e2d75b1
jbeijar <jbeijar@mios.fi>
2012-10-13 00:14:10 +0300

    Nugetified dependencies

--- Tests/Integration.cs
+++ Tests/Integration.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using System.Net.Mail;
 using Mios.Mail.Templating;
-using Should;
 using Xunit;
 
 namespace Tests {
@@ -14,14 +13,14 @@ namespace Tests {
 			var to = new MailAddress("bill@example.com");
 			MailMessage message;
 			message = template.Transform(new User { Name = "Bob" }, to, from);
-			message.To.ShouldContain(to);
-			message.From.ShouldEqual(from);
-			message.Subject.ShouldEqual("Hello Bob");
-			message.Body.ShouldEqual("Hi there Bob, how are you today?");
+			Assert.Contains(to,message.To);
+		  Assert.Equal(from,message.From);
+			Assert.Equal("Hello Bob", message.Subject);
+			Assert.Equal("Hi there Bob, how are you today?", message.Body);
 			using(var reader = new StreamReader(message.AlternateViews[0].ContentStream)) {
 				var body = reader.ReadToEnd();
-				body.ShouldContain("<h1>Hi Bob</h1>");
-				body.ShouldContain("<title>Hello Bob</title>");
+				Assert.Contains("<h1>Hi Bob</h1>", body);
+				Assert.Contains("<title>Hello Bob</title>", body);
 			}
 		}
     [Fact(Skip="Dependencies")]
@@ -32,16 +31,16 @@ namespace Tests {
 			var to = new MailAddress("bill@example.com");
 			MailMessage message;
 			message = template.Transform(new User {Name = "Bob"}, to, from);
-			message.To.ShouldContain(to);
-			message.From.ShouldEqual(from);
-			message.Subject.ShouldEqual("Hello Bob");
-			message.Body.ShouldEqual("Hi there Bob, how are you today?");
-			using(var reader = new StreamReader(message.AlternateViews[0].ContentStream)) {
-				var body = reader.ReadToEnd();
-				body.ShouldContain("<h1>Hi Bob</h1>");
-				body.ShouldContain("<title>Hello Bob</title>");
-			}
-		}
+			Assert.Contains(to, message.To);
+			Assert.Equal(from, message.From);
+			Assert.Contains("Hello Bob", message.Subject);
+      Assert.Equal("Hi there Bob, how are you today?", message.Body);
+      using(var reader = new StreamReader(message.AlternateViews[0].ContentStream)) {
+        var body = reader.ReadToEnd();
+        Assert.Contains("<h1>Hi Bob</h1>", body);
+        Assert.Contains("<title>Hello Bob</title>", body);
+      }
+    }
 		public class User {
 			public string Name { get; set; }
 		}
